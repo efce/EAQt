ec6be73e3b8a4155f86791b9db49c66e113f9e07
fc <filip.ciepiela@gmail.com>
2017-04-08 10:56:21 +0200

    simple multitrheading

--- main.cpp
+++ main.cpp
@@ -31,7 +31,10 @@ int main(int argc, char *argv[])
     translator.load("eaqt_pl",":/lang");
     a.installTranslator(&translator);
 
+    QThread thread;
     EAQtMainWindow w;
+    EAQtData::getInstance().moveToThread(&thread);
+    thread.start();
     EAQtData::getInstance().initialUpdate((EAQtUIInterface*)&w);
     w.InitialUpdate(EAQtData::getInstance());
     w.show();
