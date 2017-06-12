bd6cacf49947a6d08c5e1c0407947b24365f2738
Filip Ciepiela <filip.ciepiela@gmail.com>
2017-06-12 12:43:38 +0200

    Fix in plotting calibration when slope = 0 or intecept = inf

--- calibrationplot.cpp
+++ calibrationplot.cpp
@@ -69,30 +69,41 @@ void CalibrationPlot::update()
 {
     _plot->setVisible(true);
     _calibrationPoints->setData(_cd->xValues,_cd->yValues,false);
-    double x0 = -_cd->intercept/_cd->slope;
-    _calibrationLine->point1->setCoords(1,_cd->slope*1+_cd->intercept);
-    _calibrationLine->point2->setCoords(x0, 0);
-    _plot->rescaleAxes();
-    if ( _plot->xAxis->range().lower > x0 ) {
-        _plot->xAxis->setRangeLower(x0);
-    }
-    if ( _plot->yAxis->range().lower > 0 ) {
-        _plot->yAxis->setRangeLower(0);
-    }
-    _plot->xAxis->setLabel(tr("c / %1").arg(_cd->xUnits));
-    double spanx = _plot->xAxis->range().upper - _plot->xAxis->range().lower;
-    _plot->xAxis->setRangeLower(_plot->xAxis->range().lower - (0.1*spanx));
-    _plot->xAxis->setRangeUpper(_plot->xAxis->range().upper + (0.1*spanx));
-    double spany = _plot->yAxis->range().upper - _plot->yAxis->range().lower;
-    _plot->yAxis->setRangeLower(_plot->yAxis->range().lower - (0.1*spany));
-    _plot->yAxis->setRangeUpper(_plot->yAxis->range().upper + (0.1*spany));
-    _plot->yAxis->setLabel(tr("i / %1").arg(_cd->yUnits));
-    _plot->replot();
     QString text;
-    text = tr("r = %1 <br>").arg(_cd->correlationCoef,0,'f',4);
-    text += tr("i = %1(±%2)c + %3(±%4) <br>").arg(_cd->slope,0,'f',4).arg(_cd->slopeStdDev,0,'f',4).arg(_cd->intercept,0,'f',4).arg(_cd->interceptStdDev,0,'f',4);
-    if ( _cd->x0StdDev > -1 ) {
-        text += tr("result: (%1±%2) %3").arg(-x0,0,'f',4).arg(_cd->x0StdDev,0,'f',4).arg(_cd->xUnits);
+    if ( _cd->slope == 0 || !qIsFinite(_cd->intercept) ) {
+        text = tr("Regression line cannot be plotted.");
+        _calibrationLine->setVisible(false);
+        _plot->xAxis->setLabel(tr("c / %1").arg(_cd->xUnits));
+        _plot->yAxis->setLabel(tr("i / %1").arg(_cd->yUnits));
+        _plot->rescaleAxes();
+        _plot->replot();
+    } else {
+        double x0 = -_cd->intercept/_cd->slope;
+        _calibrationLine->point1->setCoords(1,_cd->slope*1+_cd->intercept);
+        _calibrationLine->point2->setCoords(x0, 0);
+        _calibrationLine->setVisible(true);
+        _plot->rescaleAxes();
+        if ( _plot->xAxis->range().lower > x0 ) {
+            _plot->xAxis->setRangeLower(x0);
+        }
+        if ( _plot->yAxis->range().lower > 0 ) {
+            _plot->yAxis->setRangeLower(0);
+        }
+        _plot->xAxis->setLabel(tr("c / %1").arg(_cd->xUnits));
+        double spanx = _plot->xAxis->range().upper - _plot->xAxis->range().lower;
+        _plot->xAxis->setRangeLower(_plot->xAxis->range().lower - (0.1*spanx));
+        _plot->xAxis->setRangeUpper(_plot->xAxis->range().upper + (0.1*spanx));
+        double spany = _plot->yAxis->range().upper - _plot->yAxis->range().lower;
+        _plot->yAxis->setRangeLower(_plot->yAxis->range().lower - (0.1*spany));
+        _plot->yAxis->setRangeUpper(_plot->yAxis->range().upper + (0.1*spany));
+        _plot->yAxis->setLabel(tr("i / %1").arg(_cd->yUnits));
+        _plot->replot();
+
+        text = tr("r = %1 <br>").arg(_cd->correlationCoef,0,'f',4);
+        text += tr("i = %1(±%2)c + %3(±%4) <br>").arg(_cd->slope,0,'f',4).arg(_cd->slopeStdDev,0,'f',4).arg(_cd->intercept,0,'f',4).arg(_cd->interceptStdDev,0,'f',4);
+        if ( _cd->x0StdDev > -1 ) {
+            text += tr("result: (%1±%2) %3").arg(-x0,0,'f',4).arg(_cd->x0StdDev,0,'f',4).arg(_cd->xUnits);
+        }
     }
     _te->setText(text);
     _te->setVisible(true);
