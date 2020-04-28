8758f2739fd6e5fd014415f11e4374ea5895b25f
Lukasz Gorski <lgorski84@gmail.com>
2020-04-28 17:25:16 +0200

    proceruda liczy i wyswietla na rysunku, parametry pobiera z pól

--- eaqtarplsbackgroundcorrectiondialog.cpp
+++ eaqtarplsbackgroundcorrectiondialog.cpp
@@ -10,35 +10,6 @@ EAQTArplsBackgroundCorrectionDialog::EAQTArplsBackgroundCorrectionDialog(QWidget
 { 
     ui->setupUi(this);
 
-    // default settings init
-    // Lambda
-    this->_valueLambda = this->defaultLambda;
-    ui->spinBoxLambda->setRange(this->minLambda,this->maxLambda);
-    ui->spinBoxLambda->setSingleStep(10);
-    ui->spinBoxLambda->setValue(this->defaultLambda);
-    ui->horizontalSliderLambda->setRange(this->minLambda,this->maxLambda);
-    ui->horizontalSliderLambda->setSingleStep(10);
-    ui->horizontalSliderLambda->setValue(this->defaultLambda);
-    connect(ui->horizontalSliderLambda,SIGNAL(valueChanged(int)),ui->spinBoxLambda,SLOT(setValue(int)));
-    connect(ui->spinBoxLambda,SIGNAL(valueChanged(int)),ui->horizontalSliderLambda,SLOT(setValue(int)));
-    // Ratio
-    this->_valueRatio = this->defaultRatio;
-    ui->doubleSpinBoxRatio->setRange(this->minRatio,this->maxRatio);
-    ui->doubleSpinBoxRatio->setDecimals(abs(log10(this->minRatio)));
-    ui->doubleSpinBoxRatio->setSingleStep(this->minRatio);
-    ui->doubleSpinBoxRatio->setValue(this->defaultRatio);
-    ui->horizontalSliderRatio->setRange(qPow(10,abs(log10(this->maxRatio))),qPow(10,abs(log10(this->minRatio))));
-    ui->horizontalSliderRatio->setSingleStep(qPow(10,abs(log10(this->maxRatio))));
-    ui->horizontalSliderRatio->setValue(qPow(10,abs(log10(this->defaultRatio))));
-    //qDebug() << qPow(10,abs(log10(this->maxRatio))) << " " << qPow(10,abs(log10(this->minRatio)));
-    connect(ui->horizontalSliderRatio,SIGNAL(valueChanged(int)),this,SLOT(setValueDoubleSpinBoxRatio(int)));
-    connect(ui->doubleSpinBoxRatio,SIGNAL(valueChanged(double)),this,SLOT(setValueHorizontalSliderRatio(double)));
-    // Widget Plot
-    ui->widgetPlot->xAxis->setLabel("E / mV");
-    ui->widgetPlot->yAxis->setLabel("i / µA");
-    // button Fit background
-    connect(ui->pushButtonFitBkg,SIGNAL(clicked()),this,SLOT(calculateBkg()));
-
     // main curves init
     this->_curves = EAQtData::getInstance().getCurves();
 
@@ -55,6 +26,58 @@ EAQTArplsBackgroundCorrectionDialog::EAQTArplsBackgroundCorrectionDialog(QWidget
         this->_m = 1;
     }
     this->_n = _curves->get(0)->getNrOfDataPoints();
+
+    // default settings init
+    // Lambda
+    this->_valueLambda = this->defaultLambda;
+    ui->spinBoxLambda->setRange(this->minLambda,this->maxLambda);
+    ui->spinBoxLambda->setSingleStep(10);
+    ui->spinBoxLambda->setValue(this->defaultLambda);
+    ui->horizontalSliderLambda->setRange(this->minLambda,this->maxLambda);
+    ui->horizontalSliderLambda->setSingleStep(10);
+    ui->horizontalSliderLambda->setValue(this->defaultLambda);
+    connect(ui->horizontalSliderLambda,SIGNAL(valueChanged(int)),ui->spinBoxLambda,SLOT(setValue(int)));
+    connect(ui->horizontalSliderLambda,SIGNAL(valueChanged(int)),this,SLOT(setLambdaValue(int)));
+    connect(ui->spinBoxLambda,SIGNAL(valueChanged(int)),ui->horizontalSliderLambda,SLOT(setValue(int)));
+    connect(ui->spinBoxLambda,SIGNAL(valueChanged(int)),this,SLOT(setLambdaValue(int)));
+    // Ratio
+    this->_valueRatio = this->defaultRatio;
+    ui->doubleSpinBoxRatio->setRange(this->minRatio,this->maxRatio);
+    ui->doubleSpinBoxRatio->setDecimals(abs(log10(this->minRatio)));
+    ui->doubleSpinBoxRatio->setSingleStep(this->minRatio);
+    ui->doubleSpinBoxRatio->setValue(this->defaultRatio);
+    ui->horizontalSliderRatio->setRange(qPow(10,abs(log10(this->maxRatio))),qPow(10,abs(log10(this->minRatio))));
+    ui->horizontalSliderRatio->setSingleStep(qPow(10,abs(log10(this->maxRatio))));
+    ui->horizontalSliderRatio->setValue(qPow(10,abs(log10(this->defaultRatio))));
+    //qDebug() << qPow(10,abs(log10(this->maxRatio))) << " " << qPow(10,abs(log10(this->minRatio)));
+    connect(ui->horizontalSliderRatio,SIGNAL(valueChanged(int)),this,SLOT(setValueDoubleSpinBoxRatio(int)));
+    connect(ui->doubleSpinBoxRatio,SIGNAL(valueChanged(double)),this,SLOT(setValueHorizontalSliderRatio(double)));
+    // maxIter
+    this->_maxIter = this->defaultMaxIter;
+    ui->spinBoxMaxIter->setRange(this->minMaxIter,this->maxMaxIter);
+    ui->spinBoxMaxIter->setValue(this->defaultMaxIter);
+    connect(ui->spinBoxMaxIter,SIGNAL(valueChanged(int)),this,SLOT(setMaxIterValue(int)));
+    // endPoints
+    this->_endPoints = this->defaultEndPoints;
+    this->maxEndPoints = (_n-10)/2;
+    ui->spinBoxEndPoints->setRange(this->minEndPoints,this->maxEndPoints);
+    ui->spinBoxEndPoints->setValue(this->defaultEndPoints);
+    connect(ui->spinBoxEndPoints,SIGNAL(valueChanged(int)),this,SLOT(setEndPointsValue(int)));
+    // refineW
+    this->_refineW = this->defaultRefineW;
+    ui->doubleSpinBoxRefineW->setRange(this->minRefineW,this->maxRefineW);
+    ui->doubleSpinBoxRefineW->setSingleStep(this->refineWStep);
+    ui->doubleSpinBoxRefineW->setValue(this->defaultRefineW);
+    connect(ui->doubleSpinBoxRefineW,SIGNAL(valueChanged(double)),this,SLOT(setRefineWValue(double)));
+    // Widget Plot
+    this->_plotBkg = ui->widgetPlot;
+    _plotBkg->xAxis->setLabel("E / mV");
+    _plotBkg->yAxis->setLabel("i / µA");
+    // button Fit background
+    connect(ui->pushButtonFitBkg,SIGNAL(clicked()),this,SLOT(calculateBkg()));
+    // other buttons
+    ui->pushButtonShowWithouBkg->setEnabled(false);
+    ui->pushButtonExportBkg->setEnabled(false);
 
     // main vectors init
     this->_bkg = new QVector<QVector<double>>(this->_m, QVector<double>(this->_n));
@@ -108,36 +131,62 @@ void EAQTArplsBackgroundCorrectionDialog::tests()
 
 void EAQTArplsBackgroundCorrectionDialog::plotSignals()
 {
+    _plotBkg->clearGraphs();
+
     if(_act == SELECT::all)
     {
         Curve* curve;
         int i = 0;
         while((curve = _curves->get(i)) != nullptr )
         {
-            ui->widgetPlot->addGraph();
-            ui->widgetPlot->graph(i);
-            ui->widgetPlot->graph(i)->setData(curve->getXVector(),curve->getYVector());
-            ui->widgetPlot->graph(i)->setPen(QPen(COLOR::regular));
+            _plotBkg->addGraph();
+            _plotBkg->graph(i);
+            _plotBkg->graph(i)->setData(curve->getXVector(),curve->getYVector());
+            _plotBkg->graph(i)->setPen(QPen(COLOR::regular));
             i++;
         }
     }
     else
     {
         Curve* curve = _curves->get(_act);
-        ui->widgetPlot->addGraph();
-        ui->widgetPlot->graph(0);
-        ui->widgetPlot->graph(0)->setData(curve->getXVector(),curve->getYVector());
-        ui->widgetPlot->graph(0)->setPen(QPen(COLOR::regular));
+        _plotBkg->addGraph();
+        _plotBkg->graph(0);
+        _plotBkg->graph(0)->setData(curve->getXVector(),curve->getYVector());
+        _plotBkg->graph(0)->setPen(QPen(COLOR::regular));
     }
 
-    ui->widgetPlot->rescaleAxes();
-    ui->widgetPlot->setInteractions(QCP::iRangeDrag | QCP::iRangeZoom | QCP::iSelectPlottables);
-    ui->widgetPlot->replot();
+    _plotBkg->rescaleAxes();
+    _plotBkg->setInteractions(QCP::iRangeDrag | QCP::iRangeZoom | QCP::iSelectPlottables);
+    _plotBkg->replot();
+}
+
+void EAQTArplsBackgroundCorrectionDialog::plotSignalsAndBkg()
+{
+    this->plotSignals();
+
+    int gCount = _plotBkg->graphCount();
+    for(int i = 0; i < _m; i++)
+    {
+        QVector<double> x = _curves->get(i)->getXVector();
+        QVector<double> y = _bkg[0][i];
+        _plotBkg->addGraph();
+        _plotBkg->graph(gCount);
+        _plotBkg->graph(gCount)->setData(x,y);
+        _plotBkg->graph(gCount)->setPen(QPen(COLOR::background));
+        gCount++;
+    }
+
+    _plotBkg->rescaleAxes();
+    _plotBkg->setInteractions(QCP::iRangeDrag | QCP::iRangeZoom | QCP::iSelectPlottables);
+    _plotBkg->replot();
 }
 
 void EAQTArplsBackgroundCorrectionDialog::applyArPLS()
 {
     arPLS2Ver2_initialize();
+    ui->pushButtonFitBkg->setEnabled(false);
+    ui->pushButtonShowWithouBkg->setEnabled(false);
+    ui->pushButtonExportBkg->setEnabled(false);
 
     if(_act == SELECT::all)
     {
@@ -173,12 +222,16 @@ void EAQTArplsBackgroundCorrectionDialog::applyArPLS()
     }
 
     arPLS2Ver2_terminate();
+    this->plotSignalsAndBkg();
+    ui->pushButtonFitBkg->setEnabled(true);
+    ui->pushButtonShowWithouBkg->setEnabled(true);
+    ui->pushButtonExportBkg->setEnabled(true);
 }
 
 void EAQTArplsBackgroundCorrectionDialog::tryArPLS(Curve *c, int current)
 {
     QVector<double> y = c->getYVector();
-    main_arPLS2(&y,this->_valueLambda,this->_valueRatio,100,0,0.0,current);
+    main_arPLS2(&y,_valueLambda,_valueRatio,_maxIter,_endPoints,_refineW,current);
 }
 
 emxArray_real_T *EAQTArplsBackgroundCorrectionDialog::argInit_Unboundedx1_real_T(QVector<double> *y)
@@ -213,30 +266,23 @@ void EAQTArplsBackgroundCorrectionDialog::main_arPLS2(QVector<double> *yqVect, i
     double iter;
     emxInitArray_real_T(&bkg, 1);
     emxInitArray_real_T(&weights, 1);
-    qDebug() << "init 1";
 
     // Initialize function 'arPLS2' input arguments.
     // Initialize function input argument 'y'.
     //y = argInit_Unboundedx1_real_T(yqVect);
     y = argInit_Unboundedx1_real_T(yqVect);
-    qDebug() << "init 2";
 
     // Call the entry-point 'arPLS2'.
     // arPLS(y, lambda, ratio, maxIter, includeEndsNb, threshold, bkg, weights)
     //arPLS2(y, lambda, ratio, maxIter, includeEndsNb, threshold, bkg, weights);
     arPLS2Ver2(y, lambda, ratio, maxIter, includeEndsNb, threshold, bkg, weights, &iter);
-    qDebug() << "calculated";
 
-    qDebug() << "finishing";
-    //qDebug() << bkg->numDimensions;
-    //qDebug() << bkg->size;
     qDebug() << "iterations nb: " << int32_t(iter);    
 
     for(int i = 0; i < yqVect->size(); i++) {
         this->_bkg[0][current][i] = bkg->data[i];
         this->_weights[0][current][i] = weights->data[i];
         this->_iter[0][current] = iter;
-        //qDebug() << _bkg[0].at(i);
     }
 
     emxDestroyArray_real_T(weights);
@@ -253,11 +299,33 @@ void EAQTArplsBackgroundCorrectionDialog::calculateBkg()
 void EAQTArplsBackgroundCorrectionDialog::setValueDoubleSpinBoxRatio(int val)
 {
     ui->doubleSpinBoxRatio->setValue(qPow(10,-log10(val)));
+    _valueRatio = qPow(10,-log10(val));
 }
 
 void EAQTArplsBackgroundCorrectionDialog::setValueHorizontalSliderRatio(double val)
 {
     ui->horizontalSliderRatio->setValue(qPow(10,abs(log10(val))));
+    _valueRatio = val;
+}
+
+void EAQTArplsBackgroundCorrectionDialog::setLambdaValue(int val)
+{
+    _valueLambda = val;
+}
+
+void EAQTArplsBackgroundCorrectionDialog::setMaxIterValue(int val)
+{
+    _maxIter = val;
+}
+
+void EAQTArplsBackgroundCorrectionDialog::setEndPointsValue(int val)
+{
+    _endPoints = val;
+}
+
+void EAQTArplsBackgroundCorrectionDialog::setRefineWValue(double val)
+{
+    _refineW = val;
 }
 
 
--- eaqtarplsbackgroundcorrectiondialog.h
+++ eaqtarplsbackgroundcorrectiondialog.h
@@ -29,6 +29,15 @@ public:
 private:
     Ui::EAQTArplsBackgroundCorrectionDialog *ui;
 
+    void tests();
+    void plotSignals();
+    void plotSignalsAndBkg();
+    void applyArPLS();
+    void tryArPLS(Curve *c, int current);
+    emxArray_real_T *argInit_Unboundedx1_real_T(QVector<double> *y);
+    void main_arPLS2(QVector<double> *y, int32_t lambda, double ratio, int32_t maxIter, int32_t includeEndsNb,
+                     double threshold, int current);
+
     // data size
     int32_t _m;
     int32_t _n;
@@ -42,9 +51,16 @@ private:
     QVector<QVector<double>> *_weights;
     QVector<int> *_iter;
 
+    // qCustomPlot
+    QCustomPlot *_plotBkg;
+
     // lambda and ratio param
     int32_t _valueLambda;
     double _valueRatio;
+    // other params
+    int32_t _maxIter;
+    int32_t _endPoints;
+    double _refineW;
 
     // default param values
     int32_t minLambda = 10;
@@ -53,20 +69,26 @@ private:
     double minRatio = 0.0001;
     double maxRatio = 0.1;
     double defaultRatio = 0.01;
-
-    void tests();
-    void plotSignals();
-    void applyArPLS();
-    void tryArPLS(Curve *c, int current);
-    emxArray_real_T *argInit_Unboundedx1_real_T(QVector<double> *y);
-    void main_arPLS2(QVector<double> *y, int32_t lambda, double ratio, int32_t maxIter, int32_t includeEndsNb,
-                     double threshold, int current);
+    int32_t minMaxIter = 0;
+    int32_t maxMaxIter = 1000;
+    int32_t defaultMaxIter = 100;
+    int32_t minEndPoints = 0;
+    int32_t maxEndPoints;
+    int32_t defaultEndPoints = 0;
+    double minRefineW = 0.0;
+    double maxRefineW = 0.5;
+    double defaultRefineW = 0.0;
+    double refineWStep = 0.1;
 
 
 private slots:
     void calculateBkg();
     void setValueDoubleSpinBoxRatio(int);
     void setValueHorizontalSliderRatio(double);
+    void setLambdaValue(int);
+    void setMaxIterValue(int);
+    void setEndPointsValue(int);
+    void setRefineWValue(double);
 
 };
 
